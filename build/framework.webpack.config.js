d1891b9e843b3179df565213555554c3b3d313d8
Yukipedia <pynds68@outlook.com>
2019-05-07 00:17:23 +0800

    [Webpack] 移除显示entries

--- build/framework.webpack.config.js
+++ build/framework.webpack.config.js
@@ -18,8 +18,6 @@ glob.sync("./Framework/**/*.ts").map(
 	}
 );
 
-console.log(entries);
-
 module.exports = WebpackMerge(WebpackBaseConfig, {
 	// モード値を production に設定すると最適化された状態で、
 	// development に設定するとソースマップ有効でJSファイルが出力される
