818d3abf2f60004e9917a8dd4dd6bf5f5892e2fa
Yukipedia <pynds68@outlook.com>
2019-05-06 23:57:53 +0800

    [Git] 添加忽略生成的.d.ts文件

--- Framework/cubismframeworkconfig.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const CSM_LOG_LEVEL_VERBOSE: number;
-export declare const CSM_LOG_LEVEL_DEBUG: number;
-export declare const CSM_LOG_LEVEL_INFO: number;
-export declare const CSM_LOG_LEVEL_WARNING: number;
-export declare const CSM_LOG_LEVEL_ERROR: number;
-export declare const CSM_LOG_LEVEL_OFF: number;
-export declare const CSM_LOG_LEVEL: number;
--- Framework/icubismallcator.d.ts
+++
@@ -1,8 +0,0 @@
-export declare namespace Live2DCubismFramework {
-    abstract class ICubismAllocator {
-        abstract allocate(size: number): any;
-        abstract deallocate(memory: any): void;
-        abstract allocateAligned(size: number, alignment: number): any;
-        abstract deallocateAligned(alignedMemory: any): void;
-    }
-}
